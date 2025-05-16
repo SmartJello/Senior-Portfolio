9f8888713d84181050cfc9c4259df441f372d282
SmartJello <aw195650@gmail.com>
2025-05-16 00:12:04 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+const label = document.getElementById('hover-label');
+const items = document.querySelectorAll('.emoji-item');
+
+items.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    label.textContent = item.getAttribute('data-label');
+  });
+  item.addEventListener('mouseout', () => {
+    label.textContent = '\u00A0';
+  });
+});
 
